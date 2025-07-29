f979400f95f6a28e3ce153eb4c6256bc7b26fa0d
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-07-28 23:15:29 -0300

    feat: incluindo arquivos base para o projeto

---
+++ script.sql
@@ -0,0 +1,23 @@
+-- Script de criação do banco de dados
+
+-- Script de criação da tabela usuario
+
+-- Script de criação da tabela plataforma
+
+-- Script de criação da tabela conteudo
+
+-- Script de criação da tabela interacao
+
+-- Script de inserção de dados usuario
+
+-- Script de inserção de dados plataforma
+
+-- Script de inserção de dados conteudo
+
+-- Script de inserção de dados interacao
+
+-- Script métrica conteudos_mais_consumidos
+
+-- Script métrica plataforma_maior_engajamento
+
+-- Script métrica conteudos_mais_comentados
