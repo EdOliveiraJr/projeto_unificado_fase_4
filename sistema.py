81cca87f85ba48ba384c075c46824b61049217a6
malufazenda <malu.fazenda@gmail.com>
2025-07-29 20:58:57 -0300

    feat: ajuste do nome do método de get_or_create_usuario para insert_usuario

--- sistema.py
+++ sistema.py
@@ -1,4 +1,4 @@
-def get_or_create_usuario(id_usuario):
+def insert_usuario(id_usuario):
     mycursor.execute(f"SELECT id_usuario FROM usuario WHERE id_usuario = {id_usuario};")
     if mycursor.fetchone() is None:
         mycursor.execute(f"INSERT INTO usuario (id_usuario) VALUES ({id_usuario});")
