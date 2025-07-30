c8fba0496e7fcf734fc1e34142b0f7b3a05f45b9
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-07-29 21:28:55 -0300

    feat: implementa o método createConnection para criação da conexão

--- sistema.py
+++ sistema.py
@@ -1,3 +1,29 @@
+import env_vars
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+df = pd.read_csv('interacoes_globo.csv')
+
+mydb = None
+mycursor = None
+
+def createConnection():
+  try:
+    global mydb
+    global mycursor
+    mydb = mysql.connector.connect(
+      host = env_vars.host,
+      user = env_vars.user,
+      password = env_vars.password,
+      database = env_vars.database,
+    )
+    mycursor = mydb.cursor()
+    print("Conexão criada com sucesso.")
+  except Error:
+    print(f"Erro: {Error}") 
+    
+
 def insert_usuario(id_usuario):
     mycursor.execute(f"SELECT id_usuario FROM usuario WHERE id_usuario = {id_usuario};")
     if mycursor.fetchone() is None:
